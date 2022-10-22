9c07bd921be58f56c75aff583a5ce4ce1af4ef76
Surinder Singh Matoo <surinder83singh@gmail.com>
2022-10-22 19:32:09 +0530

    FormData numeric fields

--- src/form.rs
+++ src/form.rs
@@ -1,6 +1,8 @@
 use std::collections::BTreeMap;
 use crate::result::Result;
 use crate::workflow_async_trait;
+use paste::paste;
+use std::str;
 
 pub struct Category{
     pub key:String,
@@ -23,9 +25,40 @@ where T:Into<String>{
 #[derive(Debug)]
 pub enum FormDataValue{
     String(String),
+    U8(u8),
+    U16(u16),
+    U32(u32),
+    U64(u64),
+    U128(u128),
+
     //Pubkey(String),
     //Usize(usize)
     List(Vec<String>)
+}
+
+macro_rules! num_fields {
+    ($($ident:ident)+)=>{
+        paste!{
+            $(
+            pub fn [<add_ $ident:lower>](&mut self, name:&str, value:[<$ident:lower>]){
+                self.values.insert(name.to_string(), FormDataValue::$ident(value));
+            }
+            pub fn [<get_ $ident:lower>](&self, name:&str)->Option<[<$ident:lower>]>{
+                if let Some(value) = self.values.get(name){
+                    match value{
+                        FormDataValue::$ident(value)=>{
+                            return Some(value.clone());
+                        },
+                        _=>{
+                            return None;
+                        }
+                    }
+                }
+        
+                None
+            })+
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -46,13 +79,13 @@ impl FormData{
         self.id = id;
     }
 
-    pub fn insert(&mut self, name:&str, value:FormDataValue){
+    pub fn add(&mut self, name:&str, value:FormDataValue){
         self.values.insert(name.to_string(), value);
     }
-    pub fn insert_string(&mut self, name:&str, value:String){
+    pub fn add_string(&mut self, name:&str, value:String){
         self.values.insert(name.to_string(), FormDataValue::String(value));
     }
-    pub fn insert_list(&mut self, name:&str, list:Vec<String>){
+    pub fn add_list(&mut self, name:&str, list:Vec<String>){
         self.values.insert(name.to_string(), FormDataValue::List(list));
     }
 
@@ -70,6 +103,7 @@ impl FormData{
 
         None
     }
+    num_fields!(U8 U16 U32 U64 U128);
     pub fn empty()->Self{
         Self {
             id: None,
