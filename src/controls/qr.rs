d1ca068c09acaa9551a831d1db3494ba237c9b98
Surinder Singh Matoo <surinder83singh@gmail.com>
2022-12-09 01:00:31 +0530

    Update qr.rs

--- src/controls/qr.rs
+++ src/controls/qr.rs
@@ -1,11 +1,11 @@
 use crate::prelude::*;
 use workflow_ux::result::Result;
-use qrcode::{text_to_qr_with_options, Options};
-use workflow_html::{Render, html};
+pub use qrcode::{text_to_qr_with_options, Options};
+use workflow_html::{Render, html, ElementResult, Renderables, Hooks};
 
 #[derive(Clone)]
 pub struct QRCode {
-    pub layout : ElementLayout,
+    //pub layout : ElementLayout,
     pub element : Element,
     pub code_el : Element,
     //pub text_el : Element,
@@ -18,22 +18,25 @@ impl QRCode {
     }
 
     pub fn new(
-        pane : &ElementLayout,
+        _pane : &ElementLayout,
         attributes: &Attributes,
         _docs : &Docs
     ) -> Result<Self> {
+        let content = "".to_string();
+        let text = attributes.get("qr_text").unwrap_or(&content);
+        let options = Options::from_attributes(attributes)?;
+        let control = Self::create(text, options)?;
+        Ok(control)
+    }
 
+    pub fn create(text:&str, options: Options)->Result<Self> {
         let tree = html!{
             <div class="workflow-qrcode" @qr_el>
                 <div class="qr-code" @qr_code_el></div>
             </div>
         }?;
-
-        let content = "".to_string();
-        let content = attributes.get("qr_text").unwrap_or(&content);
-        let options = Options::from_attributes(attributes)?;
         
-        let svg = text_to_qr_with_options(&content, &options)?;
+        let svg = text_to_qr_with_options(text, &options)?;
         //options.logo = None;
         //let svg2 = text_to_qr_with_options(&content, &options)?;
         
@@ -47,7 +50,7 @@ impl QRCode {
 
 
         Ok(Self { 
-            layout : pane.clone(),
+            //layout : pane.clone(),
             element,
             code_el,
             //text_el,
@@ -60,4 +63,22 @@ impl QRCode {
         Ok(())
     }
 
+}
+
+
+impl Render for QRCode {
+    fn render(&self, _w:&mut Vec<String>) -> workflow_html::ElementResult<()> {
+        Ok(())
+    }
+
+    fn render_node(
+        self,
+        parent:&mut Element,
+        _map:&mut Hooks,
+        renderables:&mut Renderables
+    )->ElementResult<()>{
+        parent.append_child(&self.element)?;
+        renderables.push(Arc::new(self));
+        Ok(())
+    }
 }
