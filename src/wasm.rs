ab307175e39bfc8ed78ba36cfe58d6af1b7223d4
Surinder Singh Matoo <surinder83singh@gmail.com>
2022-12-24 06:33:08 +0530

    inject_blob args issue

--- src/wasm.rs
+++ src/wasm.rs
@@ -18,11 +18,11 @@ pub fn load_components(flow_ux_path:&str)->Result<()>{
     Ok(())
 }
 
-pub fn load_component(flow_ux_path:&str, name:&str, cmp:&str)->Result<()>{
+pub fn load_component(flow_ux_path:&str, _name:&str, cmp:&str)->Result<()>{
     let loc = location();
     let origin = loc.origin()?;
     let js = cmp.replace("[FLOW-UX-PATH]", flow_ux_path)
                         .replace("[HOST-ORIGIN]", &origin);
-    inject_blob(name, Content::Module(js.as_bytes()))?;
+    inject_blob(Content::Module(js.as_bytes()))?;
     Ok(())
 }
